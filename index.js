d83a0bcae074b45e7f8c6cb7db913c800cd0eb1f
Fábio Nogueira Asseiro <fabioasseiro@hotmail.com>
2025-04-25 20:02:41 -0300

    size and color adjustments

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 function toggleTheme() {
     const html = document.getElementById("html");
     const theme = html.getAttribute("data-theme");
+    const icons = document.getElementsByClassName('iconFooter');
 
-    if (theme === "") {
-        html.setAttribute("data-theme", "dark");
-    } else {
-        html.setAttribute("data-theme", "");
+    const newColor = theme === "" ? "#202020" : "#ffffff";
+    const newTheme = theme === "" ? "dark" : "";
+
+    html.setAttribute("data-theme", newTheme);
+
+    // Percorre todos os ícones e aplica a nova cor
+    for (let i = 0; i < icons.length; i++) {
+        icons[i].style.fill = newColor;
     }
 }
